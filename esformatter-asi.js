3bd40c90f063bb3996fb8777b70f3b803138555f
Nathan Black <nathan@nathanblack.org>
2015-03-09 00:04:23 -0700

    WIP: Initial stab

---
+++ esformatter-asi.js
@@ -0,0 +1,42 @@
+function isWhitespace(token) {
+  return token && ~['WhiteSpace', 'Indent'].indexOf(token.type)
+}
+
+function isComment(token) {
+  return token && ~['LineComment', 'BlockComment'].indexOf(token.type)
+}
+
+function isSemicolon(token) {
+  return token && token.type === 'Punctuator' && token.value === ';'
+}
+
+exports.nodeAfter = function(node) {
+  if (~['ExpressionStatement', 'VariableDeclaration', 'ReturnStatement','ContinueStatement', 'BreakStatement'].indexOf(node.type) &&
+      isSemicolon(node.endToken)) {
+    var tokenItr = node.endToken.next,
+        semicolonToken = node.endToken
+
+    while (isWhitespace(tokenItr) || isComment(tokenItr)) {
+      tokenItr = tokenItr.next
+    }
+
+    if (tokenItr.type === 'LineBreak') {
+      var lineBreakToken = tokenItr
+      if (node.next && node.next.type === 'ExpressionStatement') {
+        tokenItr = node.next.startToken
+        while (isWhitespace(tokenItr) || isComment(tokenItr)) {
+          tokenItr = tokenItr.next
+        }
+
+        if (tokenItr && tokenItr.type === 'Punctuator' && ~['[', '(', '+', '*', '/', '-', ',', '.'].indexOf(tokenItr.value)) {
+          // ASI doesn't apply to this line as removing the semicolon would cause the lines to combine to one expression
+          return
+        }
+      }
+
+      if (semicolonToken.prev) semicolonToken.prev.next = semicolonToken.next
+      if (semicolonToken.next) semicolonToken.next.prev = semicolonToken.prev
+      node.endToken = lineBreakToken
+    }
+  }
+}
